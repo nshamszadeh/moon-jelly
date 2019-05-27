8e3e19036308976ae09ba0f71defb3c4ce15fd9b
JennyFish <jenny199810@126.com>
2019-05-26 20:39:06 -0700

    ~~

--- about.cpp
+++ about.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-	cout<<"Hi!~~~~"<<endl;
-	cout<<"Hello!!"<<endl;
+	cout<<"If you have problem with using the website, please contact JennyFish"<<endl;
+	cout<<"Email: jingjingyu@ucsb.edu"<<endl;
+	cout<<":)"<<endl;
 }
